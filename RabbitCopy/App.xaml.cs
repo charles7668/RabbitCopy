340439ae463baca5ea910dacb083d394dddf9dcd
charles7668 <charles76680119@gmail.com>
2025-08-11 10:32:20 +0800

    加入專案檔案。

---
+++ RabbitCopy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RabbitCopy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
